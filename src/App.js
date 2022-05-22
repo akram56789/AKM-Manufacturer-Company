d9a390531ab4d9788cf3ac17cefde3a34a19742f
akram56789 <akramhossain56789@gmail>
2022-05-22 13:21:14 +0600

    navber and banner done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <button class="btn btn-primary">Button</button>
+    <div className="max-w-7xl px-12 mx-auto">
+       <Navbar></Navbar>
+       <Routes>
+         <Route path='/' element={<Home></Home>}></Route>
+       </Routes>
 
     </div>
   );
