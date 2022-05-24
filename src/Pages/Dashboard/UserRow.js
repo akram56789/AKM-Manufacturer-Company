a6d72d33f3aaf21e93fb4a817def68f34d8dc0ee
akram56789 <akramhossain56789@gmail>
2022-05-25 00:24:22 +0600

    remove user button added

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { toast } from 'react-toastify';
-
+import 'react-toastify/dist/ReactToastify.css';
 const UserRow = ({ user,refetch }) => {
+    const [users, setUsers] = useState([])
     const{email , role}=user;
     const makeAdmin = () => {
+     
+
         fetch(`http://localhost:5000/user/admin/${email}`, {
             method: 'PUT',
             headers: {
+    
                 authorization: `Bearer ${localStorage.getItem('accessToken')}`
             }
         })
@@ -22,13 +26,34 @@ const UserRow = ({ user,refetch }) => {
                 }
 
             })
+
+
+            
+
+            
+    }
+    const handleDelete = _id => {
+        const proceed = window.confirm('Delete tha product !!')
+        if (proceed) {
+            const url = `http://localhost:5000/user/${email}`
+            fetch(url, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    const remaining = users.filter(u => u.email !== email)
+                    setUsers(remaining)
+                })
+  
+        }
     }
     return (
             <tr>
                 <th>1</th>
                 <td>{email}</td>
                 <td>{role!== 'admin'&& <button  className='btn btn-xs' onClick={makeAdmin}>Make Admin</button>}</td>
-                <td><button className='btn btn-xs'>Remove User</button></td>
+                <td><button onClick={() => handleDelete(users.email)} className='btn btn-xs'>Remove User</button></td>
             </tr>
     );
 };
