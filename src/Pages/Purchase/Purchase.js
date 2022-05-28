8f7b22157d4aba6fa688635eb38b3151dafdcefc
akram56789 <akramhossain56789@gmail>
2022-05-28 23:08:44 +0600

    last check

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -148,21 +148,7 @@ const Purchase = () => {
                 
               </div>
   
-              {/* <div className="  w-full max-w-xs">
-             
-                <label className="label">
-                  <span className="label-text">Minimum Order</span>
-  
-                </label>
-                <input
-                onClick={e =>  setMinimumOrder(e.target.value)}
-                  type="number"
-                  className="input input-bordered w-full max-w-xs"
-                  {...register("minimumorder")}
-                />
-  
-                
-              </div> */}
+         
               
    
    
@@ -188,68 +174,4 @@ const Purchase = () => {
 export default Purchase;
 
 
- // <div className='flex justify-center'>
-        //     <div className="card w-96 glass ">
-        //         <figure><img src={img} alt="car!" /></figure>
-        //         <div className="card-body">
-        //             <h2 className="card-title">{name}</h2>
-        //             <h4 className='text-xl'>Price</h4>
-        //             <p> Per unit: {price}$</p>
-        //             <p>Available: {quantity}</p>
-        //             <p>{description}</p>
-
-        //             <div className="card-actions justify-center mt-6 ">
-
-        //                 <label htmlFor="my-modal-6" className="btn modal-button">purchase</label>
-
-
-
-        //                 <input type="checkbox" id="my-modal-6" className="modal-toggle" />
-
-        //                 <div className="modal modal-bottom sm:modal-middle">
-
-
-        //                     <div className="modal-box">
-        //                         <label htmlFor="my-modal-6" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-        //                         <form onSubmit={handlePurchasing}>
-        //                             <h4>Name</h4>
-        //                             <input disabled defaultValue={user.displayName} type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs " />
-        //                             <h4>Email</h4>
-        //                             <input disabled defaultValue={user.email} type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-        //                             <h4>Phone</h4>
-        //                             <input type="number" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-        //                             <h4>Product Name</h4>
-        //                             <input defaultValue={name} type="text" placeholder="product name" className="input input-bordered w-full max-w-xs " />
-        //                             <h4>Price</h4>
-        //                             <input defaultValue={price} type="text" placeholder="" className="input input-bordered w-full max-w-xs  " />
-        //                             <h4>Quantity</h4>
-
-        //                             <input
-
-
-        //                                 defaultValue={quantity} type="text"
-
-
-        //                                 placeholder="Type here" className="input input-bordered w-full max-w-xs  " />
-
-        //                             <input type="submit"
-        //                                 value='Submit'
-                                        
-        //                                 className="btn btn-accent text-white font-normal mt-4	 input input-bordered w-full max-w-xs" />
-        //                         </form>
-                               
-
-        //                     </div>
-        //                 </div>
-
-
-        //             </div>
-
-
-        //         </div>
-        //     </div>
-
-
-
-
-        // </div >
+  
